9582d3b4f0160e7e4552f9fca1642e6fa9b2eba9
Andres Hincapie <ahincapie@andress-mbp-2.myfiosgateway.com>
2023-02-21 11:10:27 -0500

    new updates

--- src/App.js
+++ src/App.js
@@ -1,56 +1,64 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-
   let [input, setInput] = useState("");
   let [message, setMessage] = useState("");
 
-  useEffect(() => {
-    setMessage("Please enter your card number");
-  }, []);
+  useEffect(validCard, [input]);
 
-  function handleChange(evt){
-    setInput(evt.target.value)
+  function handleChange(evt) {
+    setInput(evt.target.value);
   }
 
-  function validCard(cardNumber){
+  function validCard() {
+    let cardNumber = input;
     let newCreditCard = [];
-    
-    if(cardNumber.length >= 4){
-      for (let i = 1; i <= cardNumber.length; i++) {
-        if(i % 2 === 0){
-          let number = parseInt(cardNumber[i - 1]) * 2;
-          number = number.toString();
-          if(number.length > 1){
-            newCreditCard[i - 1] = parseInt(number[0]) + parseInt(number[1]);
-          }else{
-            newCreditCard[i - 1] = parseInt(number);
-          }
-        }else{
-          let number = parseInt(cardNumber[i - 1]);
-          newCreditCard[i - 1] = number;
+    let creditCard = reverseCard(cardNumber);
+
+    for (let i = 1; i <= creditCard.length; i++) {
+      if (i % 2 === 0) {
+        let number = parseInt(creditCard[i - 1]) * 2;
+        number = number.toString();
+        if (number.length > 1) {
+          newCreditCard[i - 1] = parseInt(number[0]) + parseInt(number[1]);
+        } else {
+          newCreditCard[i - 1] = parseInt(number);
         }
+      } else {
+        let number = parseInt(creditCard[i - 1]);
+        newCreditCard[i - 1] = number;
       }
-      console.log(newCreditCard);
-      let sum = 0;
-      let number = newCreditCard.map(i => sum += i); 
-      console.log(number);
-      if(sum % 10 === 0){
-        setMessage("Card Valid");
-      }else{
-        setMessage("Card Not Valid");
-      }
-    } else {
-      console.log('Card Not Valid');
     }
+
+    console.log('New Credit Card = ' + newCreditCard);
+    let sum = 0;
+    let number = newCreditCard.map((i) => (sum += i));
+    console.log(number);
+
+    if(cardNumber.length === 0){
+      setMessage("Please enter your credit card number");
+    } else if (sum % 10 === 0) {
+      setMessage("Card Valid");
+    } else {
+      setMessage("Card Not Valid");
+    }
+  }
+
+  function reverseCard(cardNumber) {
+    let splitCard = cardNumber.split("");
+    console.log('Credit Card = ' + splitCard)
+
+    let reverseCardNumber = splitCard.reverse();
+    console.log('Reverse Credit Card = ' + reverseCardNumber)
+    return reverseCardNumber;
   }
 
   return (
     <div className="App">
-    <p>{message}</p>
-    <input value={input} onChange={handleChange} />
-    <button onClick={() => validCard(input)}>Submit</button>
+      <h1>The Luhn Algorithm</h1>
+      <p>{message}</p>
+      <input value={input} onChange={handleChange} />
     </div>
   );
 }
